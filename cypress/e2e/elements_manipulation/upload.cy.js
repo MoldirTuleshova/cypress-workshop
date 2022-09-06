a935b5048d571ca4ff08fbb6b89471eb2ccfabff
MoldirTuleshova <moldir.t@re-partners.com>
2022-09-07 02:59:34 +0800

    add: table, upload tests

---
+++ cypress/e2e/elements_manipulation/upload.cy.js
@@ -0,0 +1,12 @@
+describe('Upload: Upload actions', () => {
+    before('Navigate to upload page', () => {
+     cy.visit('/upload-download');
+});
+
+it('CHeck upload action', () => {
+    //Upload file
+    cy.get('#uploadFile').attachFile('pic.png');
+    //Assert that path msg is displayed
+    cy.get(#uploadedFilePath).should('have.text', 'C:\\fakepath\\pic.png');
+    });
+});   
